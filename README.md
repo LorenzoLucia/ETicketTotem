62de29b58546f98182291180f752cd3b4b5b7eb4
ghiannir <ghiannir@gmail.com>
2025-04-12 16:20:09 +0200

    Added README and service file

--- README.md
+++ README.md
@@ -1,1 +1,53 @@
 # ETicketTotem
+
+### RFID reader python utility
+
+#### Setup
+
+Create the virtual environment in your raspberry pi:
+
+`python3 -m venv venv`
+
+
+Install the dependencies:
+
+`pip install -r requirements.txt`
+
+
+Connect the RFID reader pins to the corresponding ones:
+
+![image info](img/pin_table.png)
+
+
+Start the reader utility with:
+
+`python3 read_rfid.py`
+
+Start systemd daemon with:
+`sudo cp service/rfid.service /etc/systemd/system/rfid.service`
+`sudo systemctl daemon-reload`
+`sudo systemctl enable rfid.service`
+`sudo systemctl start rfid.service`
+
+
+Check the service status with:
+`sudo systemctl status rfid.service`
+
+### Frontend
+
+#### Developement
+
+It is not recommended to develop the frontend directly in the raspberry due to its limited computing capabilities and storage. In case you choose to do it you have to install the full flutter sdk to build the application and run it with:
+`cd totem_frontend`
+`flutter run`
+
+A good option could be connecting your PC and the raspberry to the same LAN and serve the webpage with your PC using a python server.
+
+Clone this repo also on your PC(with flutter SDK installed), build the project and start the webserver in the <port> you want:
+
+`cd totem_frontend`
+`flutter build web`
+`cd build/web`
+`python3 -m http.server <port>`
+
+Open the browser inside the Raspberry, navigate to '<PC ip address>:<port>'
--- read_rfid.py
+++ read_rfid.py
@@ -1,38 +1,30 @@
 import RPi.GPIO as GPIO
 from mfrc522 import SimpleMFRC522
-import time
-
-reader = SimpleMFRC522()
-
-try:
-    print("Place your RFID tag or card near the reader...")
-    id, text = reader.read()
-    print(f"RFID Tag ID: {id}")
-    print(f"Stored Text: {text}")
-    publish_data(topic, id)
-
-except KeyboardInterrupt:
-    print("Exiting...")
-finally:
-    GPIO.cleanup()
-    time.sleep(1)
-    client.disconnect()
-
 import asyncio
 import websockets
 
-async def publish_numbers(websocket):
-    number = 1234567890  # Starting number
-    input("Press Enter to start sending numbers...")
-    
-    await websocket.send(str(number))
-    print(f"Sent: {number}")
+GPIO.cleanup()
 
-        # number += 1
-        # await asyncio.sleep(1)
+reader = SimpleMFRC522()
+
+async def publish_numbers(websocket):
+
+    try:
+        print("Place your RFID tag or card near the reader...")
+        id, text = reader.read()
+        print(f"RFID Tag ID: {type(id)}")
+        print(f"Stored Text: {text}")
+    except KeyboardInterrupt:
+        print("Exiting...")
+    finally:
+        GPIO.cleanup()
+
+    await websocket.send(str(id))
+    print(f"Sent on socket: {id}")
+
 
 async def main():
-    async with websockets.serve(lambda ws: publish_numbers(ws), "localhost", 8765):
+    async with websockets.serve(lambda ws: publish_numbers(ws), "localhost", 9001):
         await asyncio.Future()  # Run forever
 
 asyncio.run(main())
